cdaa23eeb3723e7a4e9401fd41f8d8a68aeedc2f
Luther <luther.kroe@gmail.com>
2020-07-28 07:16:16 +0800

    Add test for manually constructed library alias

--- src/test/java/convex/core/lang/AliasTest.java
+++ src/test/java/convex/core/lang/AliasTest.java
@@ -3,9 +3,11 @@ package convex.core.lang;
 import static convex.core.lang.TestState.*;
 import static convex.test.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
+import convex.core.data.Address;
 import convex.core.data.Maps;
 
 public class AliasTest {
@@ -16,5 +18,19 @@ public class AliasTest {
 	@Test public void testWipeAlias() {
 		Context<?> ctx=step("(def *aliases* {})");
 		assertUndeclaredError(step(ctx,"count"));
+	}
+	
+	@Test public void testLibraryAlias() {
+		Context<?> ctx=step("(def lib (deploy (fn [] (def foo 100))))");
+		Address libAddress=eval(ctx,"lib");
+		assertNotNull(libAddress);
+		
+		// no alias should exist yet
+		assertUndeclaredError(step(ctx,"foo"));
+		
+		ctx=step(ctx,"(def *aliases* (assoc *aliases* 'mylib lib))");
+		
+		// Alias should now work
+		assertEquals(100L,evalL(ctx,"mylib/foo"));
 	}
 }
