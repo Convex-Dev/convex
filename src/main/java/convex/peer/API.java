0be2379586309c6cc95688f62a2f8f78717dbbe5
Luther <luther.kroe@gmail.com>
2020-10-06 19:19:34 +0800

    Remove old code

--- src/main/java/convex/peer/API.java
+++ src/main/java/convex/peer/API.java
@@ -1,7 +1,5 @@
 package convex.peer;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -9,13 +7,8 @@ import java.util.logging.Logger;
 import convex.core.Init;
 import convex.core.data.Keyword;
 import convex.core.data.Keywords;
-import convex.core.lang.Reader;
 import convex.core.store.Stores;
-import convex.core.transactions.ATransaction;
-import convex.core.transactions.Invoke;
 import convex.core.util.Utils;
-import convex.net.Connection;
-import convex.net.ResultConsumer;
 
 /**
  * Class providing a simple API to a peer Server.
@@ -58,57 +51,9 @@ public class API {
 			server.launch();
 			return server;
 		} catch (Throwable t) {
+			log.warning("Error launching peer: "+t.getMessage());
 			t.printStackTrace();
 			throw Utils.sneakyThrow(t);
 		}
-	}
-
-	private static final ResultConsumer resultConsumer = new ResultConsumer() {
-
-		@Override
-		protected void handleResult(long id, Object m) {
-			log.info("RESULT RECEIVED: " + m);
-		}
-
-		@Override
-		protected void handleError(long id, Object code, Object msg) {
-			log.info("ERROR RECEIVED: " + code + " : " + msg);
-		}
-
-	};
-
-	/**
-	 * Demo of key API functions for user application
-	 * 
-	 * @param args
-	 * @throws InterruptedException
-	 * @throws IOException
-	 */
-	public static void main(String... args) throws InterruptedException, IOException {
-		Server s = launchPeer();
-		InetSocketAddress hostAddress = s.getHostAddress();
-		log.info("Launched server at address: " + hostAddress);
-
-		// connect with the specified consumer for callbacks
-		Connection c = Connection.connect(hostAddress, resultConsumer, Stores.getGlobalStore());
-
-		// send a query as the HERO user
-		long queryID = c.sendQuery(Reader.read("(+ 1 2)"), Init.HERO);
-		log.info("Sent query ith ID: " + queryID);
-
-		// send a transaction as the HERO user
-		// we need the next sequence number for transaction to be valid
-		long seq = s.getPeer().getConsensusState().getAccount(Init.HERO).getSequence();
-		ATransaction tx = Invoke.create(seq + 1, Reader.read("*balance*"));
-		long txID = c.sendTransaction(Init.HERO_KP.signData(tx));
-		log.info("Sent transaction with ID: " + txID);
-
-		// wait a bit
-		Thread.sleep(5000);
-
-		// shutdown
-		log.info("Triggering shutdown after 5s");
-		s.close();
-		Thread.sleep(500);
 	}
 }
