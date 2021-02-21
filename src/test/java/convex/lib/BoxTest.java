cde35bcab82e9f58aa7a487f875b60f41370c45f
Mike <mike@mikera.net>
2021-02-21 13:22:46 +0800

    Generic asset tests for boxes

--- src/test/java/convex/lib/BoxTest.java
+++ src/test/java/convex/lib/BoxTest.java
@@ -1,16 +1,23 @@
 package convex.lib;
 
+import static convex.core.lang.TestState.eval;
 import static convex.core.lang.TestState.step;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
+import convex.core.crypto.AKeyPair;
+import convex.core.data.AVector;
 import convex.core.data.Address;
+import convex.core.data.Set;
+import convex.core.data.Sets;
 import convex.core.data.Symbol;
 import convex.core.data.Syntax;
+import convex.core.data.prim.CVMLong;
 import convex.core.lang.Context;
 import convex.core.lang.Reader;
 import convex.core.lang.TestState;
@@ -20,37 +27,65 @@ import convex.test.Testing;
 
 public class BoxTest {
 	private static final Symbol nSym=Symbol.create("box");
-
-	private static Context<?> loadBox() {
+	
+	static final AKeyPair KP1=AKeyPair.generate();
+	static final AKeyPair KP2=AKeyPair.generate();
+	
+	static final Address BOX;
+	
+	private static final Context<?> CTX;
+	
+	static {
 		Context<?> ctx=TestState.INITIAL_CONTEXT.fork();
 		try {
 			ctx=ctx.deployActor(Reader.read(Utils.readResourceAsString("libraries/box.con")));
 			Address nft=(Address) ctx.getResult();
 			assert (ctx.getDepth()==0):"Invalid depth: "+ctx.getDepth();
-			String importS="(import "+nft+" :as "+nSym.getName()+")";
+			String importS="(def box (import "+nft+" :as "+nSym.getName()+"))";
 			ctx=step(ctx,importS);
-			assertFalse(ctx.isExceptional());
+			BOX=(Address)ctx.getResult();
+			assertNotNull(BOX);
 			
 			ctx=ctx.define(nSym, Syntax.create(nft));
 		} catch (IOException e) {
+			e.printStackTrace();
 			throw new Error(e);
 		}
 		ctx=step(ctx,"(import convex.asset :as asset)");
-		return ctx;
-	}
-	
-	private static final Context<?> ctx;
-	
-	static {
-		ctx=loadBox();
+		CTX=ctx;
 	}
 	
 	@Test public void testSetup() {
-		assertTrue(ctx.lookup(nSym).getValue() instanceof Address);
+		assertTrue(CTX.lookup(nSym).getValue() instanceof Address);
 	}
 	
 	@Test public void testScript1() {
-		Context<?> c=Testing.runTests(ctx,"contracts/box/test1.con");
+		Context<?> c=Testing.runTests(CTX,"contracts/box/test1.con");
 		Assertions.assertNotError(c);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test public void testAssetAPI() {
+		Context<?> ctx=CTX.fork();
+		ctx=step(ctx,"(def total (map (fn [v] (call box (create-box))) [1 2 3 4]))");
+		AVector<CVMLong> v=(AVector<CVMLong>) ctx.getResult();
+		assertEquals(4,v.count());
+		CVMLong b1=v.get(0);
+		
+		// Test balance
+		assertEquals(Set.create(v.toCellArray()),eval(ctx,"(asset/balance box)"));
+		
+		// Create test Users
+		ctx=ctx.createAccount(KP1.getAccountKey());
+		Address user1=(Address) ctx.getResult();
+		ctx=ctx.createAccount(KP2.getAccountKey());
+		Address user2=(Address) ctx.getResult();
+		
+		ctx=step(ctx,"(asset/transfer "+user1+" [box (set (next total))])");
+		ctx=step(ctx,"(asset/transfer "+user2+" [box #{"+b1+"}])");
+		assertEquals(Sets.of(b1),ctx.getResult());
+		
+		AssetTest.doAssetTests(ctx, BOX, user1, user2);
+
 	}
 }
