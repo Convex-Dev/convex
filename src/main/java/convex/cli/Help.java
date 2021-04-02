f50b5bd80aa27c4442f99cfa4ec24d6af66603d2
Mike <mike@mikera.net>
2021-04-02 08:00:48 +0800

    More CLI refactoring

--- src/main/java/convex/cli/Help.java
+++ src/main/java/convex/cli/Help.java
@@ -27,21 +27,11 @@ public class Help {
 			String cmd=commands.get(0);
 			switch (cmd) {
 			case "peer": {
+				Peer.buildPeerHelp(sb,commands);
 				break;
 			}
 			case "key": {
-				sb.append("Usage: convex [OPTIONS] key COMMAND ... \n");
-				sb.append('\n');
-				sb.append("Commands:\n");
-				sb.append(CLIUtils.buildTable(
-						"gen",     "Generate a new private key pair."));
-				sb.append('\n');		
-				sb.append("Options:\n");
-				sb.append(CLIUtils.buildTable(
-						"-h, --help",           "Display help for the command '"+cmd+"'.",
-						"-k, --keystore FILE",  "Use the specified keystore. Defaults to the keystore specified in config file, or '~/.convex/keystore otherwise'",
-						"-c, --config FILE",    "Use the specified config file. Defaults to '~/.convex/config'",
-						"-p, --passphrase", "Specify a passphrase to protect key. If not specified, will be promtped."));
+				Key.buildKeyHelp(sb, commands);
 				break;
 			}
 			
---
+++ src/main/java/convex/cli/Peer.java
@@ -0,0 +1,25 @@
+package convex.cli;
+
+import java.util.List;
+
+public class Peer {
+
+	public static void buildPeerHelp(StringBuilder sb, List<String> commands) {
+		String cmd=commands.get(0);
+
+		sb.append("Usage: convex [OPTIONS] "+cmd+" COMMAND ... \n");
+		sb.append('\n');
+		sb.append("Commands:\n");
+		sb.append(CLIUtils.buildTable(
+				"start",     "Start a peer server."));
+		sb.append('\n');		
+		sb.append("Options:\n");
+		sb.append(CLIUtils.buildTable(
+				"-h, --help",           "Display help for the command '"+cmd+"'.",
+				"-k, --keystore FILE",  "Use the specified keystore. Defaults to the keystore specified in config file, or '~/.convex/keystore otherwise'",
+				"-c, --config FILE",    "Use the specified config file. Defaults to '~/.convex/config'",
+				"-p, --passphrase", "Specify a passphrase to protect key. If not specified, will be promtped."));
+
+	}
+
+}
