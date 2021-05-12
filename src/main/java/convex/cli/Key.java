63f56f9e6eb72aa8e70bacc132947b2a9f297ba1
bill <billbsing@gmail.com>
2021-05-12 14:19:02 +0800

    remove old cli and replace

--- src/main/java/convex/cli/Key.java
+++ src/main/java/convex/cli/Key.java
@@ -1,73 +1,45 @@
-package convex.cli;
+package convex.cli2;
 
-import java.io.File;
-import java.security.KeyStore;
-import java.security.Provider;
-import java.security.Security;
-import java.util.List;
-import java.util.Properties;
-import java.util.Scanner;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ParentCommand;
 
-import convex.core.crypto.PFXTools;
+/**
+*
+* Convex key sub commands
+*
+*/
+@Command(name="key",
+	mixinStandardHelpOptions=true,
+	description="Manage local Convex key store.")
+public class Key implements Runnable {
 
-public class Key {
+	@ParentCommand
+	private Main parent;
 
-	static void buildKeyHelp(StringBuilder sb, List<String> commands) {
-		String cmd=commands.get(0);
-
-		sb.append("Usage: convex [OPTIONS] "+cmd+" COMMAND ... \n");
-		sb.append('\n');
-		sb.append("Commands:\n");
-		sb.append(CLIUtils.buildTable(
-				"gen",     "Generate a new private key pair.",
-				"list",    "List available key pairs."));
-		sb.append('\n');
-		sb.append("Options:\n");
-		sb.append(CLIUtils.buildTable(
-				"-h, --help",           "Display help for the command '"+cmd+"'.",
-				"-k, --keystore FILE",  "Use the specified keystore. Defaults to the keystore specified in config file, or '~/.convex/keystore otherwise'",
-				"-c, --config FILE",    "Use the specified config file. Defaults to '~/.convex/config'",
-				"-p, --passphrase", "Specify a passphrase to protect key. If not specified, will be promtped."));
+	// key generate command
+	@Command(name="generate",
+		aliases={"g","gen"},
+		mixinStandardHelpOptions=true,
+		description="Generate a new private key pair.")
+	void generate() {
+		System.out.println("key generate");
 	}
 
-	static int runKey(List<String> commands, Properties config) {
-		if (commands.size()<=1) {
-			return Help.runHelp(commands);
-		}
-
-		String cmd=commands.get(1);
-		if ("gen".equals(cmd)) {
-			return runKeyGen(config);
-		} else {
-			System.out.println("Unrecognised key command: "+cmd);
-			return 1;
-		}
+	// key list command
+	@Command(name="list",
+		aliases={"l","li"},
+		mixinStandardHelpOptions=true,
+		description="List available key pairs.")
+	void list() {
+		System.out.println("key list");
 	}
 
-	private static int runKeyGen(Properties config) {
-		String keyPath=CLIUtils.expandTilde(config.getProperty("keystore"));
-		String passPhrase=config.getProperty("passphrase");
-		File keyFile = new File(keyPath);
-		for ( Provider provider : Security.getProviders()) {
-            System.out.println(provider.getName());
-		}
-		if (!keyFile.exists()) {
-            if (passPhrase == null) {
-                System.out.print("Enter in your pass phrase: ");
-                Scanner scanner = new Scanner(System.in);
-                passPhrase=scanner.nextLine();
-            }
-			System.out.println("Creating key store: "+keyFile);
-			try {
-				PFXTools.createStore(keyFile, passPhrase);
-			} catch (Throwable e) {
-				// TODO Auto-generated catch block
-				System.out.println("Unable to create keystore");
-				e.printStackTrace();
-				return 1;
-			}
-		}
-		return 0;
+	public void run() {
+		// sub command run with no command provided
+		CommandLine.usage(new Key(), System.out);
 	}
 
 }
--- src/main/java/convex/cli/Peer.java
+++ src/main/java/convex/cli/Peer.java
@@ -1,9 +1,7 @@
-package convex.cli;
+package convex.cli2;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 import convex.core.data.Keyword;
 import convex.core.data.Keywords;
@@ -11,61 +9,58 @@ import convex.core.util.Utils;
 import convex.peer.API;
 import convex.peer.Server;
 
-public class Peer {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ParentCommand;
 
-	public static void buildPeerHelp(StringBuilder sb, List<String> commands) {
-		String cmd=commands.get(0);
+/**
+*
+* Convex peer sub commands
+*
+*/
+@Command(name="peer",
+	mixinStandardHelpOptions=true,
+	description="Operates a local peer.")
+public class Peer implements Runnable {
 
-		sb.append("Usage: convex [OPTIONS] "+cmd+" COMMAND ... \n");
-		sb.append('\n');
-		sb.append("Commands:\n");
-		sb.append(CLIUtils.buildTable(
-				"start",     "Start a peer server."));
-		sb.append('\n');		
-		sb.append("Options:\n");
-		sb.append(CLIUtils.buildTable(
-				"-h, --help",           "Display help for the command '"+cmd+"'.",
-				"-k, --keystore FILE",  "Use the specified keystore. Defaults to the keystore specified in config file, or '~/.convex/keystore otherwise'",
-				"-c, --config FILE",    "Use the specified config file. Defaults to '~/.convex/config'",
-				"--port", "Specify a port to run the peer."));
+	@ParentCommand
+	private Main parent;
 
-	}
+	@Option(names={"-p", "--port"},
+		description="Specify a port to run the local peer.")
+	private int port;
 
-	public static int runPeer(List<String> commands, Properties config) {
-		if (commands.size()<=1) {
-			return Help.runHelp(commands);
-		}
-		
-		String cmd=commands.get(1);
-		if ("start".equals(cmd)) {
-			return runStart(config);
-		} else {
-			System.out.println("Unrecognised key command: "+cmd);
-			return 1;
-		}
-	}
-
-	private static int runStart(Properties config) {
+	// peer start command
+	@Command(name="start",
+		mixinStandardHelpOptions=true,
+		description="Starts a peer server.")
+	void start() {
 		System.out.println("Starting peer...");
-		
+
 		// Parse peer config
-		Map<Keyword,Object> pc=new HashMap<>();
-		
-		String port=config.getProperty("port");
-		if (port!=null) {
-			pc.put(Keywords.PORT, Utils.toInt(port));
+		Map<Keyword,Object> peerConfig=new HashMap<>();
+
+		if (port!=0) {
+			peerConfig.put(Keywords.PORT, port);
 		}
-		
-		Server s = API.launchPeer(pc);
+
+		Server s = API.launchPeer(peerConfig);
 		System.out.println("Peer started at "+s.getHostAddress() +" with public key "+s.getPeer().getPeerKey());
 		while (true) {
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				System.out.println("Peer interrupted!");
-				return 1;
+				return;
 			}
 		}
+	}
+
+	public void run() {
+		// sub command run with no command provided
+		CommandLine.usage(new Peer(), System.out);
 	}
 
 }
