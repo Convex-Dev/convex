dce41c3f60e76f9d02daa5491cd51c1ea6ffc3d0
Mike <mike@mikera.net>
2021-05-12 09:02:50 +0800

    Add Double and Byte types

---
+++ src/main/java/convex/core/data/type/Byte.java
@@ -0,0 +1,35 @@
+package convex.core.data.type;
+
+import convex.core.data.ACell;
+import convex.core.data.prim.CVMByte;
+
+/**
+ * Type that represents CVM Byte values
+ */
+public final class Byte extends ANumericType {
+
+	/**
+	 * Singleton runtime instance
+	 */
+	public static final Byte INSTANCE = new Byte();
+
+	private Byte() {
+		
+	}
+	
+	@Override
+	public boolean check(ACell value) {
+		return value instanceof CVMByte;
+	}
+	
+	@Override
+	public String toString () {
+		return "Byte";
+	}
+
+	@Override
+	public boolean allowsNull() {
+		return false;
+	}
+
+}
---
+++ src/main/java/convex/core/data/type/Double.java
@@ -0,0 +1,35 @@
+package convex.core.data.type;
+
+import convex.core.data.ACell;
+import convex.core.data.prim.CVMDouble;
+
+/**
+ * Type that represents CVM Double values
+ */
+public final class Double extends ANumericType {
+
+	/**
+	 * Singleton runtime instance
+	 */
+	public static final Double INSTANCE = new Double();
+
+	private Double() {
+		
+	}
+	
+	@Override
+	public boolean check(ACell value) {
+		return value instanceof CVMDouble;
+	}
+	
+	@Override
+	public String toString () {
+		return "Double";
+	}
+
+	@Override
+	public boolean allowsNull() {
+		return false;
+	}
+
+}
