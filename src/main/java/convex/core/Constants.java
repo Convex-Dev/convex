2c2a6d423b46a15aee2b5c72a45a19a0c2570a5b
Mike <mike@mikera.net>
2021-06-01 07:23:28 +0800

    Longer default client timeout

--- src/main/java/convex/core/Constants.java
+++ src/main/java/convex/core/Constants.java
@@ -46,7 +46,7 @@ public class Constants {
 	 */
 	public static final long MEMORY_OVERHEAD = 64;
 
-	public static final long DEFAULT_CLIENT_TIMEOUT = 5000;
+	public static final long DEFAULT_CLIENT_TIMEOUT = 10000;
 
 	/**
 	 * Allowance for initial user / peer accounts
