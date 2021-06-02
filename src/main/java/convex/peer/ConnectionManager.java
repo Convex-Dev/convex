7a99c489325aadcd96254fd29a3d8955fb5213b3
bill <billbsing@gmail.com>
2021-06-02 11:43:43 +0800

    remove redundent imports

--- src/main/java/convex/peer/ConnectionManager.java
+++ src/main/java/convex/peer/ConnectionManager.java
@@ -2,9 +2,7 @@ package convex.peer;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.logging.Logger;
 
 import convex.core.data.Keyword;
