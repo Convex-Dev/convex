60cb33901348680da067f33ce88fb3d2f35f80a3
Mike <mike@mikera.net>
2021-06-07 18:40:09 +0800

    Test edits

--- src/test/java/convex/core/lang/OpsTest.java
+++ src/test/java/convex/core/lang/OpsTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
