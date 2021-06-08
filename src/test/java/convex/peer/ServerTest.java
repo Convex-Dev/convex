f1b753a3109bca22531df39c1353f91ccc5f7343
bill <billbsing@gmail.com>
2021-06-08 10:30:54 +0800

    fix tests

--- src/test/java/convex/peer/ServerTest.java
+++ src/test/java/convex/peer/ServerTest.java
@@ -60,12 +60,12 @@ public class ServerTest {
 	static final AKeyPair peerKeyPair;
 	static final AKeyPair keyPair;
 	static final Address HERO;
-	
+
 	static {
 		peerKeyPair = TestState.FIRST_PEER_KEYPAIR;
 		keyPair=TestState.HERO_KP;
 		HERO=Init.HERO;
-		
+
 		Map<Keyword, Object> config = new HashMap<>();
 		config.put(Keywords.PORT, 0); // create new port
 		config.put(Keywords.STATE, Init.createState());
@@ -74,7 +74,7 @@ public class ServerTest {
 
 		server = API.launchPeer(config);
 	}
-	
+
 	private static final Logger log = Logger.getLogger(ServerTest.class.getName());
 
 	private HashMap<Long, Object> results = new HashMap<>();
@@ -85,7 +85,7 @@ public class ServerTest {
 			log.finer(id+ " : "+Utils.toString(value));
 			results.put(id, value);
 		}
-		
+
 		@Override
 		protected synchronized void handleError(long id, Object code, Object message) {
 			log.finer(id+ " ERR: "+Utils.toString(code));
@@ -96,23 +96,23 @@ public class ServerTest {
 	@Test
 	public void testServerConnect() throws IOException, InterruptedException {
 		InetSocketAddress hostAddress=server.getHostAddress();
-		
+
 		// Connect to Peer Server using the current store for the client
-		Connection pc = Connection.connect(hostAddress, handler, Stores.current());
+		Connection pc = Connection.connect(hostAddress, handler, Stores.current(), null);
 		AVector<CVMLong> v = Vectors.of(1l, 2l, 3l);
 		long id1 = pc.sendQuery(v,Init.HERO);
 		Utils.timeout(5000, () -> results.get(id1) != null);
 		assertEquals(v, results.get(id1));
 	}
-	
-// Commented out because it's slow....	
+
+// Commented out because it's slow....
 //	@Test
 //	public void testServerFlood() throws IOException, InterruptedException {
 //		InetSocketAddress hostAddress=server.getHostAddress();
 //		// This is a test of flooding a client connection with async messages. Should eventually throw an IOExcepion
 //		// from backpressure and *not* bring down the server.
 //		Convex convex=Convex.connect(hostAddress, Init.VILLAIN,Init.VILLAIN_KP);
-//		
+//
 //		Object cmd=Reader.read("(def tmp (inc tmp))");
 //		assertThrows(IOException.class, ()-> {
 //			for (int i=0; i<1000000; i++) {
@@ -120,78 +120,78 @@ public class ServerTest {
 //			}
 //		});
 //	}
-	
+
 	@Test public void testBalanceQuery() throws IOException {
 		Convex convex=Convex.connect(server.getHostAddress(),TestState.VILLAIN,TestState.VILLAIN_KP);
-		
+
 		// test the connection is still working
 		assertNotNull(convex.getBalance(Init.VILLAIN));
 	}
-	
+
 	@Test
 	public void testConvexAPI() throws IOException, InterruptedException, ExecutionException, TimeoutException {
 		Convex convex=Convex.connect(server.getHostAddress(),TestState.VILLAIN,TestState.VILLAIN_KP);
-		
+
 		Future<convex.core.Result> f=convex.query(Symbols.STAR_BALANCE);
 		convex.core.Result f2=convex.querySync(Symbols.STAR_ADDRESS);
-		
+
 		assertEquals(Init.VILLAIN,f2.getValue());
 		assertCVMEquals(TestState.STATE.getBalance(TestState.VILLAIN),f.get().getValue());
 	}
-	
+
 	@Test
 	public void testMissingData() throws IOException, InterruptedException {
-		
+
 		InetSocketAddress hostAddress=server.getHostAddress();
-		
+
 		// Connect to Peer Server using the current store for the client
 		AStore store=Stores.current();
-		Connection pc = Connection.connect(hostAddress, handler, store);
+		Connection pc = Connection.connect(hostAddress, handler, store, null);
 		State s=server.getPeer().getConsensusState();
 		Hash h=s.getHash();
-		
+
 		boolean sent=pc.sendMissingData(h);
 		assertTrue(sent);
-		
+
 		Thread.sleep(200);
 		Ref<State> ref=Ref.forHash(h);
 		assertNotNull(ref);
 	}
-	
+
 	@Test
 	public void testAcquireBelief() throws IOException, InterruptedException, ExecutionException, TimeoutException, BadSignatureException {
 		synchronized(ServerTest.server) {
 			// Stores.setCurrent(Stores.getGlobalStore()); // not needed?
 			InetSocketAddress hostAddress=server.getHostAddress();
-			
+
 			// Connect to Peer Server using the current store for the client
 			// SignedData<Belief> s=server.getPeer().getSignedBelief();
 			// Hash h=s.getHash();
 			//System.out.println("SignedBelief Hash="+h);
 			//System.out.println("testAcquireBelief store="+Stores.current());
-			
+
 			Convex convex=Convex.connect(hostAddress, HERO, TestState.HERO_KP);
-			
+
 			Future<Result> statusFuture=convex.requestStatus();
 			Result status=statusFuture.get(10000,TimeUnit.MILLISECONDS);
 			assertFalse(status.isError());
 			AVector<?> v=status.getValue();
 			Hash h=(Hash)v.get(0);
-			
+
 			Future<SignedData<Belief>> acquiror=convex.acquire(h);
 			SignedData<Belief> ab=acquiror.get(10000,TimeUnit.MILLISECONDS);
 			assertTrue(ab.getValue() instanceof Belief);
 			assertEquals(h,ab.getHash());
 		}
 	}
-	
+
 	@Test
 	public void testServerTransactions() throws IOException, InterruptedException {
 		synchronized(ServerTest.server) {
 			InetSocketAddress hostAddress=server.getHostAddress();
-			
+
 			// Connect to Peer Server using the current store for the client
-			Connection pc = Connection.connect(hostAddress, handler, Stores.current());
+			Connection pc = Connection.connect(hostAddress, handler, Stores.current(), null);
 			long s=server.getPeer().getConsensusState().getAccount(HERO).getSequence();
 			Address addr=HERO;
 			AKeyPair kp=keyPair;
@@ -203,15 +203,15 @@ public class ServerTest {
 			long id5 = pc.sendTransaction(kp.signData(Call.create(addr, s+5, Init.REGISTRY_ADDRESS, Symbols.FOO, Vectors.of(Maps.empty()))));
 			long id6bad = pc.sendTransaction(kp.signData(Invoke.create(Init.VILLAIN, s+6, Reader.read("(def a 1)"))));
 			long id6 = pc.sendTransaction(kp.signData(Invoke.create(addr, s+6, Reader.read("foo"))));
-			
+
 			long last=id6;
-			
+
 			assertTrue(last>=0);
 			assertTrue(!pc.isClosed());
-			
+
 			// wait for results to come back
 			assertFalse(Utils.timeout(10000, () -> results.containsKey(last)));
-			
+
 			AVector<CVMLong> v = Vectors.of(1l, 2l, 3l);
 			assertCVMEquals(v, results.get(id1));
 			assertCVMEquals(2L, results.get(id2));
@@ -223,7 +223,7 @@ public class ServerTest {
 			assertEquals(ErrorCodes.UNDECLARED, results.get(id6));
 		}
 	}
-	
+
 
 
 
