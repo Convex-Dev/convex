02e0d698e3e7fc7c7396d28d9e31bdbb339a473b
bill <billbsing@gmail.com>
2021-07-16 09:14:27 +0800

    add logback

--- src/main/java/convex/cli/Main.java
+++ src/main/java/convex/cli/Main.java
@@ -13,10 +13,10 @@ import convex.core.crypto.AKeyPair;
 import convex.core.crypto.PFXTools;
 import convex.core.data.Address;
 import convex.core.init.AInitConfig;
+
+import ch.qos.logback.classic.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-// import org.slf4j.impl.SimpleLogger;
-// import org.slf4j.simple.SimpleLoggerFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -50,7 +50,7 @@ import picocli.CommandLine.ScopeType;
 
 public class Main implements Runnable {
 
-	private static Logger log;
+	private static Logger log = LoggerFactory.getLogger(Main.class);
 
 
 	private static CommandLine commandLine;
@@ -89,8 +89,8 @@ public class Main implements Runnable {
 
     @Option(names={ "-v", "--verbose"},
 		scope = ScopeType.INHERIT,
-		description="Show more verbose log information.")
-	private boolean verbose;
+		description="Show more verbose log information. You can increase verbosity by using multiple -v or -vvv")
+	private boolean[] verbose = new boolean[0];
 
 
 	@Override
@@ -119,18 +119,15 @@ public class Main implements Runnable {
 			System.err.println("unable to parse arguments " + t);
 		}
 
-		System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "warn");
-		if (verbose) {
-			System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "trace");
-		}
+		ch.qos.logback.classic.Logger parentLogger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
 
-		Main.log = LoggerFactory.getLogger(Main.class);
-		log.info("starting..");
-		log.trace("trace");
-		log.debug("debug");
-		log.info("info");
-		log.warn("warn");
-		log.error("error");
+		Level[] verboseLevels = {Level.INFO, Level.DEBUG, Level.TRACE, Level.ALL};
+
+		parentLogger.setLevel(Level.WARN);
+		if (verbose.length > 0 && verbose.length <= verboseLevels.length) {
+			parentLogger.setLevel(verboseLevels[verbose.length]);
+			log.info("set level to {}", parentLogger.getLevel());
+		}
 
 		int result = 0;
 		try {
