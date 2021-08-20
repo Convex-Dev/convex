0e402dd9ed65b5619c7ae6c6eef680b8a97d6c50
Mike <mike@mikera.net>
2021-08-20 10:15:13 +0800

    Tidy warnings

--- convex-cli/src/main/java/convex/cli/PeerStart.java
+++ convex-cli/src/main/java/convex/cli/PeerStart.java
@@ -2,22 +2,17 @@ package convex.cli;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-
-import convex.cli.peer.PeerManager;
-import convex.cli.peer.SessionItem;
-import convex.core.Belief;
-import convex.core.State;
-import convex.core.crypto.AKeyPair;
-import convex.core.data.Address;
-import convex.core.data.Hash;
-import convex.core.data.SignedData;
-import convex.core.store.AStore;
-import convex.core.store.Stores;
-import etch.EtchStore;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import convex.cli.peer.PeerManager;
+import convex.cli.peer.SessionItem;
+import convex.core.crypto.AKeyPair;
+import convex.core.data.Address;
+import convex.core.store.AStore;
+import convex.core.store.Stores;
+import etch.EtchStore;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Option;
