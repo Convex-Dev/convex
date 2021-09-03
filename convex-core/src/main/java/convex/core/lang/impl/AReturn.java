4dfe9ed6cf975340f9777ef5ab51c24439ec74c9
Mike <mike@mikera.net>
2021-09-03 09:58:52 +0800

    Delete all files

--- convex-core/src/main/java/convex/core/lang/impl/AReturn.java
+++
@@ -1,8 +0,0 @@
-package convex.core.lang.impl;
-
-/**
- * Abstract base class for exceptional returns
- */
-public abstract class AReturn extends AExceptional {
-
-}
--- convex.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -jar convex-cli/target/convex-cli.jar %*
-exit /b %errorlevel%
