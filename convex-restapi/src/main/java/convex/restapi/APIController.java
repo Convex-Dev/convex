24aac3cd6accadcdfc59e85d8cb17e40c3304714
bill <billbsing@gmail.com>
2022-04-19 16:19:28 +0800

    using blade framework

--- convex-restapi/src/main/java/convex/restapi/APIController.java
+++ convex-restapi/src/main/java/convex/restapi/APIController.java
@@ -1,19 +1,16 @@
 package convex.restapi;
 
-import java.util.concurrent.atomic.AtomicLong;
+import com.blade.mvc.annotation.GetRoute;
+import com.blade.mvc.annotation.Path;
+import com.blade.mvc.annotation.PathParam;
+import com.blade.mvc.http.Response;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Path
 public class APIController {
 
-	private static final String template = "Hello, world";
-	private final AtomicLong counter = new AtomicLong();
-
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new String(template + " " + name);
+	@GetRoute("/api/v1/accounts/:address")
+	public void getAccount(Response response, @PathParam String address) {
+		response.text("account info " + address);
 	}
 }
---
+++ convex-restapi/src/main/java/convex/restapi/Main.java
@@ -0,0 +1,10 @@
+package convex.restapi;
+
+import com.blade.Blade;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Blade.of().start();
+	}
+}
