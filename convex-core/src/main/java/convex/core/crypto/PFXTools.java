a2a7997f8eebe7c9367cbc4804366e25db15fef7
Mike <mike@mikera.net>
2022-07-05 21:52:42 +0100

    Fix FileNotFoundException for loadKeyStore

--- convex-core/src/main/java/convex/core/crypto/PFXTools.java
+++ convex-core/src/main/java/convex/core/crypto/PFXTools.java
@@ -2,7 +2,6 @@ package convex.core.crypto;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
