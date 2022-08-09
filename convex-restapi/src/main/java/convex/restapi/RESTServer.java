0653c6f78560e47c601e9d5e6d73a40d6cf7c30c
Luther <luther.kroe@gmail.com>
2022-08-09 11:17:09 +0100

    Stub for Javalin API server

--- convex-restapi/src/main/java/convex/restapi/RESTServer.java
+++ convex-restapi/src/main/java/convex/restapi/RESTServer.java
@@ -1,25 +1,52 @@
 package convex.restapi;
 
 import convex.api.Convex;
+import convex.api.ConvexLocal;
 import convex.peer.Server;
+import io.javalin.Javalin;
 
 public class RESTServer {
 
 	private Server server;
 	private Convex convex;
+	private Javalin app;
+	
+	private RESTServer() {
+		app=Javalin.create(config->{
+		});
+	}
 
 	/**
-	 * Create a RESTServer connected to a local Convex Peer Server instance
+	 * Create a RESTServer connected to a local Convex Peer Server instance. Defaults to using the Peer Controller account.
+	 * 
 	 * @param server
 	 * @return
 	 */
 	public RESTServer create(Server server) {
 		RESTServer newServer=new RESTServer();
 		newServer.server=server;
+		newServer.convex=ConvexLocal.create(server, server.getPeerController(), server.getKeyPair());
 		return newServer;
+	}
+	
+	public void start() {
+		app.start();
+		
+	}
+	
+	public void stop() {
+		app.close();
 	}
 	
 	public Convex getConvex() {
 		return convex;
+	}
+	
+	/**
+	 * Gets the local Server instance, or null if not a local connection. 
+	 * @return
+	 */
+	public Server getServer() {
+		return server;
 	}
 }
