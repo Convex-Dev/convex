6ed3c1f370fb3125dab8ef1007cf8c50e310080c
Luther <luther.kroe@gmail.com>
2022-08-11 17:54:11 +0100

    More edits for OpenAPI schema

--- convex-restapi/src/main/java/convex/restapi/RESTServer.java
+++ convex-restapi/src/main/java/convex/restapi/RESTServer.java
@@ -22,98 +22,106 @@ public class RESTServer {
 	private Server server;
 	private Convex convex;
 	private Javalin app;
-	
+
 	private RESTServer() {
-		app=Javalin.create(config->{
-			 config.enableWebjars();
-			 config.enableCorsForAllOrigins();
-			 config.addStaticFiles(staticFiles -> {
-				    staticFiles.hostedPath = "/";                   // change to host files on a subpath, like '/assets'
-				    staticFiles.directory = "/public";              // the directory where your files are located
-				    staticFiles.location = Location.CLASSPATH;      // Location.CLASSPATH (jar) or Location.EXTERNAL (file system)
-				    staticFiles.precompress = false;                // if the files should be pre-compressed and cached in memory (optimization)
-				    staticFiles.aliasCheck = null;                  // you can configure this to enable symlinks (= ContextHandler.ApproveAliases())
-				    staticFiles.skipFileFunction = req -> false;    // you can use this to skip certain files in the dir, based on the HttpServletRequest
+		app = Javalin.create(config -> {
+			config.enableWebjars();
+			config.enableCorsForAllOrigins();
+			config.addStaticFiles(staticFiles -> {
+				staticFiles.hostedPath = "/"; // change to host files on a subpath, like '/assets'
+				staticFiles.directory = "/public"; // the directory where your files are located
+				staticFiles.location = Location.CLASSPATH; // Location.CLASSPATH (jar) or Location.EXTERNAL (file
+															// system)
+				staticFiles.precompress = false; // if the files should be pre-compressed and cached in memory
+													// (optimization)
+				staticFiles.aliasCheck = null; // you can configure this to enable symlinks (=
+												// ContextHandler.ApproveAliases())
+				staticFiles.skipFileFunction = req -> false; // you can use this to skip certain files in the dir, based
+																// on the HttpServletRequest
 			});
 		});
-		 
+
 		addAPIRoutes();
 	}
-    
+
 	private void addAPIRoutes() {
-		app.post("/api/v1/createAccount",this::createAccount);
+		app.post("/api/v1/createAccount", this::createAccount);
 
 	}
-	
-		public void createAccount(Context ctx) {
-			Map<String,Object> req;
-			try {
-				req=JSON.toMap(ctx.body()); 
-			} catch (Exception e) {
-				throw new BadRequestResponse(jsonError("Invalid JSON body"));
-			}
-			Object key=req.get("accountKey");
-			if (key==null) throw new BadRequestResponse(jsonError("Expected JSON body containing 'accountKey' field"));
-			
-			AccountKey pk=AccountKey.parse(key);
-			if (pk==null) throw new BadRequestResponse(jsonError("Unable to parse accountKey: "+key));
-			
-			Address a;
-			try {
-				a = convex.createAccountSync(pk);
-			} catch (TimeoutException e) {
-				throw new ServiceUnavailableResponse(jsonError("Timeout in request"));
-			} catch (IOException e) {
-				throw new InternalServerErrorResponse(jsonError(e.getMessage()));
-			}
-			ctx.result("{\"address\": "+a.toLong()+"}");
+
+	public void createAccount(Context ctx) {
+		Map<String, Object> req;
+		try {
+			req = JSON.toMap(ctx.body());
+		} catch (Exception e) {
+			throw new BadRequestResponse(jsonError("Invalid JSON body"));
 		}
-		
-	 
+		Object key = req.get("accountKey");
+		if (key == null)
+			throw new BadRequestResponse(jsonError("Expected JSON body containing 'accountKey' field"));
+
+		AccountKey pk = AccountKey.parse(key);
+		if (pk == null)
+			throw new BadRequestResponse(jsonError("Unable to parse accountKey: " + key));
+
+		Address a;
+		try {
+			a = convex.createAccountSync(pk);
+		} catch (TimeoutException e) {
+			throw new ServiceUnavailableResponse(jsonError("Timeout in request"));
+		} catch (IOException e) {
+			throw new InternalServerErrorResponse(jsonError(e.getMessage()));
+		}
+		ctx.result("{\"address\": " + a.toLong() + "}");
+	}
+
 	private static String jsonError(String string) {
-		return "{\"error\":\""+string+"\"}";
+		return "{\"error\":\"" + string + "\"}";
 	}
 
 	/**
-	 * Create a RESTServer connected to a local Convex Peer Server instance. Defaults to using the Peer Controller account.
+	 * Create a RESTServer connected to a local Convex Peer Server instance.
+	 * Defaults to using the Peer Controller account.
 	 * 
 	 * @param server
 	 * @return
 	 */
 	public static RESTServer create(Server server) {
-		RESTServer newServer=new RESTServer();
-		newServer.server=server;
-		newServer.convex=ConvexLocal.create(server, server.getPeerController(), server.getKeyPair());
+		RESTServer newServer = new RESTServer();
+		newServer.server = server;
+		newServer.convex = ConvexLocal.create(server, server.getPeerController(), server.getKeyPair());
 		return newServer;
 	}
-	
+
 	/**
-	 * Create a RESTServer connected to a Convex Client instance. Defaults to using the Peer Controller account.
+	 * Create a RESTServer connected to a Convex Client instance. Defaults to using
+	 * the Peer Controller account.
 	 * 
 	 * @param server
 	 * @return
 	 */
 	public RESTServer create(Convex convex) {
-		RESTServer newServer=new RESTServer();
-		newServer.convex=convex;
+		RESTServer newServer = new RESTServer();
+		newServer.convex = convex;
 		return newServer;
 	}
-	
+
 	public void start() {
 		app.start();
-		
+
 	}
-	
+
 	public void stop() {
 		app.close();
 	}
-	
+
 	public Convex getConvex() {
 		return convex;
 	}
-	
+
 	/**
-	 * Gets the local Server instance, or null if not a local connection. 
+	 * Gets the local Server instance, or null if not a local connection.
+	 * 
 	 * @return
 	 */
 	public Server getServer() {
