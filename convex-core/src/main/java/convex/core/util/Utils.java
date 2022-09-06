c7864b8051f1b2e03ef0cca92a4e08b7cb2047c6
Mike <mike@mikera.net>
2022-09-06 14:52:39 +0100

    Tidy imports

--- convex-core/src/main/java/convex/core/util/Utils.java
+++ convex-core/src/main/java/convex/core/util/Utils.java
@@ -21,7 +21,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
