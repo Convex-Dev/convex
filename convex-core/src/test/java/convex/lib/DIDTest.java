c11c36ec88c6ce1cdad0b94c039bf1e5df6feff4
Mike <mike@mikera.net>
2022-11-22 18:06:35 +0000

    Experiments with DID registry

--- convex-core/src/test/java/convex/lib/DIDTest.java
+++ convex-core/src/test/java/convex/lib/DIDTest.java
@@ -1,16 +1,24 @@
 package convex.lib;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
+import convex.core.data.ACell;
 import convex.core.data.Address;
 import convex.core.lang.ACVMTest;
+import convex.core.lang.Context;
 
 public class DIDTest extends ACVMTest {
 
 	@Test public void testLibrary() {
 		Address did=eval("(import convex.did)");
 		assertNotNull(did);
+	}
+	
+	@Test public void testResolve() {
+		Context<ACell> ctx=step("(import convex.did :as did)");
+		assertNull(eval(ctx,"(did/resolve *address*)"));
 	}
 }
