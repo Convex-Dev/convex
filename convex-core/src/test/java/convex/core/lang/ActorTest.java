63a71a999a49a3143a70dcfd30b5e6c57829b8ee
Mike <mike@mikera.net>
2023-04-28 15:47:02 +0100

    Extra tests for scoped calls

--- convex-core/src/test/java/convex/core/lang/ActorTest.java
+++ convex-core/src/test/java/convex/core/lang/ActorTest.java
@@ -19,5 +19,29 @@ public class ActorTest extends ACVMTest {
 		assertEquals(eval(c,"[a1 :s :foo]"),eval(c,"(call [a1 :foo] (check))"));
 		assertEquals(eval(c,"[a1 :s nil]"),eval(c,"(call a1 (check))"));
 		assertNull(eval(c,"(do (call [a1 :foo] (check)) *scope*)"));
+		
+		c=step(c,"(def a2 (deploy '(defn ^:callable? nest [t] [*scope* (call t (check)) *scope*])))");
+		assertNotError(c);
+		
+		assertEquals(eval(c,"[nil [a1 :s :foo] nil]"),eval(c,"(call a2 (nest [a1 :foo]))"));
+		assertEquals(eval(c,"[nil [a1 :s nil] nil]"),eval(c,"(call a2 (nest [a1 nil]))"));
+		assertEquals(eval(c,"[nil [a1 :s nil] nil]"),eval(c,"(call a2 (nest a1))"));
+		assertEquals(eval(c,"[:foo [a1 :s nil] :foo]"),eval(c,"(call [a2 :foo] (nest a1))"));
+		assertEquals(eval(c,"[:foo [a1 :s :bar] :foo]"),eval(c,"(call [a2 :foo] (nest [a1 :bar]))"));
+	}
+	
+	@Test 
+	public void testBadScopedCalls() {
+		Context<?> c=context();
+		
+		c=step(c,"(def a1 (deploy '(defn ^:callable? check [] [*caller* *address* *scope*])))");
+		assertNotError(c);
+		
+		// scope vector too small
+		assertCastError(step(c,"(call [] (check))"));
+		assertCastError(step(c,"(call [a1] (check))"));
+		
+		// scope vector too big
+		assertCastError(step(c,"(call [a1 nil nil] (check))"));
 	}
 }
