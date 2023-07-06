f8c76ef201d1709d208780a7f7b995a060930daa
Mike <mike@mikera.net>
2023-07-06 10:54:37 +0100

    Add extra tests for Etch store consistency

--- convex-core/src/main/java/etch/EtchUtils.java
+++ convex-core/src/main/java/etch/EtchUtils.java
@@ -5,13 +5,25 @@ import java.io.IOException;
 import convex.core.util.Utils;
 
 public class EtchUtils {
+	
+	public static FullValidator getFullValidator() {
+		return new FullValidator();
+	}
 
-	public static IEtchIndexVisitor fullValidator=new IEtchIndexVisitor() {
-
+	public static class FullValidator implements IEtchIndexVisitor {
+		public long visited=0;
+		public long entries=0;
+		public long empty=0;
+		public long values=0;
+		public long indexPtrs=0;
 		@Override
 		public void visit(Etch e, int level, int[] digits, long indexPointer) {
+			visited++;
+			
 			try {
 				int isize=e.indexSize(level);
+				entries+=isize;
+				
 				if (isize<=0) fail("Bad index size:"+isize);
 				
 				for (int i=0; i<isize; i++) {
@@ -20,6 +32,33 @@ public class EtchUtils {
 					long type=e.extractType(slot);
 					
 					if ((ptr|type)!=slot) fail("Inconsistent slot code?!?");
+					
+					if (slot==0) {
+						empty++;
+					} else if (type!=Etch.PTR_INDEX) {
+						values++;
+					} else {
+						indexPtrs++;
+					}
+					
+					if (type==Etch.PTR_START) {
+						int ipp=(i+1)%isize; // next slot
+						long nextSlot=e.readSlot(indexPointer, ipp);
+						if (e.extractType(nextSlot)!=Etch.PTR_CHAIN) {
+							fail("Invalid slot after chain start: "+Utils.toHexString(nextSlot));
+						}
+					}
+					
+					if (type==Etch.PTR_CHAIN) {
+						int imm=(i+isize-1)%isize; // prev slot
+						long prevSlot=e.readSlot(indexPointer, imm);
+						long pt=e.extractType(prevSlot);
+						if (!((pt==Etch.PTR_CHAIN)||(pt==Etch.PTR_START))) {
+							fail("Invalid slot before chain entry: "+Utils.toHexString(prevSlot));
+						}
+					}
+
+					
 				}
 			} catch (IOException e1) {
 				throw Utils.sneakyThrow(e1);
