da72077945abc7bc59e97576416f489d148cd565
mikera <mike@mikera.net>
2024-04-22 13:36:41 +0100

    Edits for better identicon security (suspicious value colouring)

--- convex-gui/src/main/java/convex/gui/components/Identicon.java
+++ convex-gui/src/main/java/convex/gui/components/Identicon.java
@@ -11,13 +11,13 @@ import javax.swing.border.BevelBorder;
 import convex.core.data.AArrayBlob;
 import convex.core.data.Blobs;
 import convex.core.data.Hash;
+import convex.core.data.Keyword;
 import convex.core.data.prim.CVMLong;
 import convex.core.lang.RT;
-import convex.core.util.Utils;
 import convex.gui.utils.Toolkit;
 
 /**
- * A simple identicon for visualising hash values.
+ * A simple identicon component for visualising hash values.
  */
 @SuppressWarnings("serial")
 public class Identicon extends JLabel {
@@ -26,24 +26,38 @@ public class Identicon extends JLabel {
 	
 	public BufferedImage createImage(AArrayBlob data, int renderSize) {
 		int SIZE = size;
-		if (data==null) return  new BufferedImage(renderSize, renderSize, BufferedImage.TYPE_INT_RGB);
+		if ((data==null)||(data.isEmpty())) return  new BufferedImage(renderSize, renderSize, BufferedImage.TYPE_INT_RGB);
+		long n=data.count(); // must be one byte at least
 		
 		BufferedImage bi = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_RGB);
-		byte[] bs = Blobs.ensureZeroBasedArray(data);
 
 		int[] cols=new int[4];
 		
 		// last 12 bytes define colours, 3 bytes per colour, we mask the first byte for ARGB
 		for (int i=0; i<4; i++) {
-			int col=0xFF000000|(Utils.readInt(bs, 20+i*3-1));
+			int r=(int) (0xff&(data.byteAt(Math.floorMod(n-12+i*3+0,n))));
+			int g=(int) (0xff&(data.byteAt(Math.floorMod(n-12+i*3+1,n))));
+			int b=(int) (0xff&(data.byteAt(Math.floorMod(n-12+i*3+2,n))));
+			
+			// XOR with red high bit, tends to make invalid / non-random data look suspicious
+			int col=0x800000^((r<<16)|(g<<8)|b);
+			// int col=((r<<16)|(g<<8)|b);
 			cols[i]=col;
 		}
 		
-		// First 20 bytes define bitmap. Take 2 bits for 
+		// number of pixels for each row
+		int width=((SIZE+1)/2);
+		
+		// First 20 bytes define bitmap. Take 2 bits for each 
 		for (int y = 0; y < SIZE; y++) {
-			for (int x = 0; x <= SIZE/2; x++) {
-				int i = (x + y * (1+SIZE/2))%(20*4); // 4 2-byte segments per byte
-				int bits = Utils.extractBits(bs, 2, 2 * i); // take 2 bits for colour index
+			for (int x = 0; x <= width; x++) {
+				int i = (x + y * width); // 4 2-byte segments per byte
+				
+				int byteIndex=i/4;
+				if (byteIndex>=n) break;
+				
+				byte b=data.byteAt(byteIndex);
+				int bits = 0x03&(b>>(2*(3-(i%4)))); // take 2 bits for colour index, high bits first
 				int rgb = cols[bits];
 				bi.setRGB(x, y, rgb);
 				bi.setRGB(SIZE-x-1, y, rgb);
@@ -70,7 +84,6 @@ public class Identicon extends JLabel {
 
 	public Identicon(AArrayBlob a) {
 		super();
-		if ((a!=null)&&a.count()<32) throw new IllegalArgumentException("Blob too short for Identicon, must be 32+ bytes. Maybe use a Hash?");
 		size=7;
 		ImageIcon icon=createIcon(a, 36);
 		setIcon(icon);
@@ -81,7 +94,24 @@ public class Identicon extends JLabel {
 	public static class IdenticApp extends AbstractGUI{
 		public IdenticApp() {
 			setLayout(new FlowLayout());
-			for (int i=0; i<256; i++) {
+			
+			// Identicons for Strings
+			for (int i=0; i<64; i++) {
+				add(new Identicon(CVMLong.create(i).getHash().toCVMString(100).toFlatBlob()));
+			}
+			
+			// Identicons for small encodings
+			for (int i=0; i<64; i++) {
+				add(new Identicon(Keyword.create(Integer.toString(i)).getEncoding()));
+			}
+			
+			// Identicons for blobs with same values
+			for (int i=0; i<64; i++) {
+				add(new Identicon(Blobs.createFilled(i*4, 32).toFlatBlob()));
+			}
+
+			// Identicons for Hashes / random data
+			for (int i=0; i<64; i++) {
 				add(new Identicon(Hash.compute(CVMLong.create(i).getHash())));
 			}
 			add(new Identicon(null));
