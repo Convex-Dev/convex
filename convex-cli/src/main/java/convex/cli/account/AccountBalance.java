4aa891e1d5534a1b58fa9ba8f02ec7a7d2fc61f4
Mike <mike@mikera.net>
2024-09-08 18:50:01 +0100

    Make CLI `convex account balance` work better

--- convex-cli/src/main/java/convex/cli/account/AccountBalance.java
+++ convex-cli/src/main/java/convex/cli/account/AccountBalance.java
@@ -1,14 +1,14 @@
 package convex.cli.account;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import convex.api.Convex;
+import convex.cli.CLIError;
 import convex.cli.Constants;
-import convex.cli.Main;
+import convex.cli.ExitCodes;
 import convex.core.Result;
 import convex.core.data.ACell;
+import convex.core.data.AVector;
 import convex.core.data.Address;
+import convex.core.lang.RT;
 import convex.core.lang.Reader;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -28,36 +28,51 @@ import picocli.CommandLine.ParentCommand;
 	mixinStandardHelpOptions=true,
 	description="Get account balance of the specified address.")
 public class AccountBalance extends AAccountCommand {
-	private static final Logger log = LoggerFactory.getLogger(AccountBalance.class);
  
 	@ParentCommand
 	private Account accountParent;
 
-	@Parameters(paramLabel="address",
-	description="Address of the account to get the balance .")
-	private Long addressNumber;
-
-	@Option(names={"-t", "--timeout"},
-		description="Timeout in miliseconds.")
-	private long timeout = Constants.DEFAULT_TIMEOUT_MILLIS;
-
+	@Parameters(paramLabel="addresses",
+	description="Address(es) of account to query balance for.")
+	private String[] addresses;
 
 	@Override
 	public void execute() throws InterruptedException {
-		Main mainParent = accountParent.mainParent;
-
-		if (addressNumber == 0) {
-			log.warn("You need to provide a valid address number");
+		if (addresses==null) {
+			if (verbose()>=2) {
+				informWarning("No address(es) specified.");
+				showUsage();
+			}
 			return;
-		}
-
-		Address address = Address.create(addressNumber);
+		}		
+		
+		int n = addresses.length;
 
 		Convex convex = peerMixin.connect();
-		String queryCommand = "(balance "+address+")";
-		ACell message = Reader.read(queryCommand);
-		Result result = convex.querySync(message);
-		mainParent.printResult(result);
-
+		try {
+			StringBuilder sb=new StringBuilder();
+			sb.append("(map balance [");
+			for (int i=0; i<n; i++) {
+				Address addr=Address.parse(addresses[i]);
+				if (addr==null) {
+					throw new CLIError(ExitCodes.USAGE,"Address not valid: "+addresses[i]);
+				}
+				sb.append(addr);
+			}
+			sb.append("])");
+			ACell message = Reader.read(sb.toString());
+			Result result = convex.querySync(message);
+			if (result.isError()) {
+				throw new CLIError("Balance query failed: " +result.toString());
+			} else {
+				AVector<ACell> v=RT.ensureVector(result.getValue());
+				if (v==null) throw new CLIError(ExitCodes.DATAERR,"Invalid result, expected vector of balances: "+result);
+				for (int i=0; i<n; i++) {
+					println(v.get(i));
+				}
+			}
+		} finally  {
+			convex.close();
+		}
 	}
 }
