d6305f9b1f27d24d9624b03f7c2f1fbfa9809b46
mikera <mike@mikera.net>
2024-09-11 18:36:09 +0100

    Add CLI account test, fix JUnit dependencies

---
+++ convex-cli/src/test/java/convex/cli/account/AccountTest.java
@@ -0,0 +1,60 @@
+package convex.cli.account;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import convex.cli.CLTester;
+import convex.cli.ExitCodes;
+import convex.core.crypto.AKeyPair;
+import convex.core.data.AccountStatus;
+import convex.core.data.Address;
+import convex.core.data.Keyword;
+import convex.core.data.Keywords;
+import convex.peer.API;
+import convex.peer.ConfigException;
+import convex.peer.LaunchException;
+import convex.peer.Server;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class AccountTest {
+
+	static final AKeyPair kp=AKeyPair.createSeeded(345634);
+	protected static Server SERVER;
+	protected static String PORT;
+
+	@BeforeAll
+	public static void setupPeer() throws LaunchException, ConfigException, InterruptedException {
+		HashMap<Keyword,Object> config=new HashMap<>();
+		config.put(Keywords.KEYPAIR, kp);
+		SERVER=API.launchPeer(config);
+		PORT=Integer.toString(SERVER.getPort());
+
+	}
+	
+	@Test
+	public void testBalance() {
+		CLTester tester;;
+		
+		tester =  CLTester.run(
+				"account", 
+				"balance",
+				"--port",PORT,
+				"11"
+			);
+		tester.assertExitCode(ExitCodes.SUCCESS);
+		AccountStatus as=SERVER.getPeer().getConsensusState().getAccount(Address.create(11));
+		assertEquals(""+as.getBalance(),tester.getOutput().trim());
+	}
+	
+	@AfterAll
+	public static void closePeer() {
+		SERVER.shutdown();
+	}
+}
