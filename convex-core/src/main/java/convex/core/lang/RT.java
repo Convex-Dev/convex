0f1026d1bf8d2746e98306d03f7ec1b83fa053f2
mikera <mike@mikera.net>
2025-03-14 11:27:31 +0000

    Javadoc fix

--- convex-core/src/main/java/convex/core/lang/RT.java
+++ convex-core/src/main/java/convex/core/lang/RT.java
@@ -748,7 +748,7 @@ public class RT {
 	 * 
 	 * @param o     Object to attempt to convert to a Vector.
 	 * @param start start index to slice
-	 * @end end index to slice
+	 * @param  end index to slice
 	 * @return AVector instance, or null if not convertible
 	 */
 	@SuppressWarnings("unchecked")
