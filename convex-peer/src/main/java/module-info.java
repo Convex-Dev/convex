c75b9752fd07e0b665da7559afbba5f0201e54c5
Mike <mike@mikera.net>
2025-04-23 10:53:58 +0800

    Dep updates

--- convex-peer/src/main/java/module-info.java
+++ convex-peer/src/main/java/module-info.java
@@ -7,9 +7,8 @@ module convex.peer {
 	requires transitive convex.core;
 	requires java.net.http;
 	requires org.slf4j;
-	requires transitive io.netty.all;
+	requires transitive io.netty.common;
 	requires io.netty.transport;
 	requires io.netty.buffer;
-	requires io.netty.common;
 	requires io.netty.codec;
 }
