0278f25efe7a4bd74aa1ec6db84ea8a1950400a7
mikera <mike@mikera.net>
2025-07-21 10:53:19 +0100

    Cursors docs

--- convex-core/src/main/java/convex/lattice/Cursors.java
+++ convex-core/src/main/java/convex/lattice/Cursors.java
@@ -5,10 +5,22 @@ import convex.core.lang.RT;
 
 public class Cursors {
 
+	/**
+	 * Create a root cursor with the given value
+	 * @param <V> Type of cursor value
+	 * @param a Any object to be converted to CVM type
+	 * @return New cursor instance
+	 */
 	public static <V extends ACell> Root<V> of(Object a) {
 		return create(RT.cvm(a));
 	}
 
+	/**
+	 * Create a root cursor with the given value
+	 * @param <V> Type of cursor value
+	 * @param a Any compatible CVM value
+	 * @return New cursor instance
+	 */
 	private static <V extends ACell> Root<V> create(V value) {
 		return Root.create(value);
 	}
