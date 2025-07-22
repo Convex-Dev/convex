4dd4919e0a27cbc93de1f564c94fe0f4d92d923b
mikera <mike@mikera.net>
2025-07-22 16:58:45 +0100

    More FileUtils tests

--- convex-core/src/test/java/convex/core/util/FileUtilsTest.java
+++ convex-core/src/test/java/convex/core/util/FileUtilsTest.java
@@ -2,17 +2,29 @@ package convex.core.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
 
 public class FileUtilsTest {
 
 	
-	@Test public void testGetPath() {
+	@Test public void testGetHomePath() {
 		String home=System.getProperty("user.home");
 		assertNotNull(home);
 		
-		assertEquals(home,FileUtils.getFile("~").toString());
-		assertEquals(home,FileUtils.getPath("~").toString());
+		File file=FileUtils.getFile("~");
+		assertEquals(home,file.toString());
+		assertTrue(file.isAbsolute());
+		assertTrue(file.isDirectory());
+		
+		Path path=FileUtils.getPath("~");
+		assertEquals(home,path.toString());
+		assertTrue(path.isAbsolute());
+		assertTrue(Files.isDirectory(path));
 	}
 }
