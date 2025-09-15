ab81e35e526a1f10b094155c3c2f7394f862e556
mikera <mike@mikera.net>
2025-09-15 22:26:18 +0100

    Add a caching cursor implementation

--- convex-core/src/main/java/convex/lattice/ACachedView.java
+++ convex-core/src/main/java/convex/lattice/ACachedView.java
@@ -3,19 +3,18 @@ package convex.lattice;
 import convex.core.data.ACell;
 
 /**
- * Abstract base class for cursor implementations that cache a value
- * @param <V>
+ * Abstract base class for cursor implementations that cache a value.
+ * 
+ * This class provides a framework for implementing different caching strategies
+ * by delegating cache management to concrete subclasses. Subclasses must implement
+ * the caching logic.
+ * 
+ * @param <V> The type of value being cached
  */
 public abstract class ACachedView<V extends ACell> extends AView<V> {
 
 	protected ACachedView(ACursor<V> source) {
 		super(source);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public V get() {
-		return source.get();
 	}
 
 
